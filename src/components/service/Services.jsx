f0561737ba9d1fb695aa3829b0129798f3fd81d9
Ifeanyi Beniah Onyebueke <ifeanyionyebueke.ben@gmail.com>
2022-08-20 00:02:52 +0200

    implement about,experience and service component

--- src/components/service/Services.jsx
+++ src/components/service/Services.jsx
@@ -1,7 +1,128 @@
 import React from "react";
+import {AiOutlineCheck} from "react-icons/ai"
 import "./service.css";
 const Services = () => {
-  return <section id="services">Services</section>;
+  return <section id="services">
+    
+<h5>What I offer</h5> 
+<h2>Services</h2>  
+<div className="container services__container">
+  <article className="service">
+    <div className="service__head">
+      <h3>UI/UX Design</h3>
+    </div>
+    <ul className="service__list">
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+    </ul>
+  </article>
+  {/* ==================CONTENT CREATION ========= */}
+  <article className="service">
+    <div className="service__head">
+      <h3>Content Creation</h3>
+    </div>
+    <ul className="service__list">
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+    </ul>
+  </article>
+  {/* ======WEB DEV========= */}
+  <article className="service">
+    <div className="service__head">
+      <h3>Web Development</h3>
+    </div>
+    <ul className="service__list">
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+      <li>
+      <AiOutlineCheck className="service__list-icon"/>
+      <p>Lorem ipsum dolor sit amet  adipisicing elit.</p>
+
+      </li>
+    </ul>
+  </article>
+
+</div>
+</section>;
 };
 
 export default Services;
